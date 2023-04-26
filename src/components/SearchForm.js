0821262f767544aee874ec67f12a71af037c5815
Nyaromollo <omolloenter@gmail.com>
2023-04-26 17:43:19 +0300

    Completed Assignment

---
+++ src/components/SearchForm.js
@@ -0,0 +1,18 @@
+import {useState} from "react"
+export default function SearchForm({onSearching}){
+    const[search,setSearch]=useState("")
+    function handleChange(e){
+        e.preventDefault()
+        setSearch(e.target.value)
+        onSearching(search)
+        
+    }
+    return (
+            <form onChange={handleChange} id="search-form">
+                <input value={search} id="search" placeholder="Search Transanctions..." type={"text"}/>
+                <button>
+                    {/* <FontAwesomeIcon icon={faSearch}/> */}
+                    </button>
+            </form>
+    )
+}
